a5f21faa39e71bcbaabf4f90970455aa5e691690
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2022-12-05 00:06:28 -0800

    minor tweaks to ui and caching

--- src/routes/projects/+page.ts
+++ src/routes/projects/+page.ts
@@ -1,8 +1,13 @@
 import type { PageLoad } from './$types';
 import type { Repos } from '$lib/types/repos';
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ fetch, setHeaders }) => {
 	const res = await fetch(`/api/repos/1`);
 	const repos: Repos = await res.json();
+
+	setHeaders({
+		'cache-control': res.headers.get('cache-control') ?? "cache-control': 'public, max-age=3600"
+	});
+
 	return { repos };
 };
